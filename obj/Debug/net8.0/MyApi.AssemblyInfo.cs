d7f8d7420c8e081feba8febf0905cb99f0bf0ee3
miryam hana <m0534198438@gmail.com>
2024-12-17 21:18:18 +0200

    12/17/2024

--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05a050ffa4dc4fb2aa0627e506f908ff17ec9c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01e0c804ea3208ba8935603e917b1d5d5a6ada1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
